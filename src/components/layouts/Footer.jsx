7fc0d3f765543db20b8be45c15149de3bc3e431f
agusmena <agusmena60@mail.com>
2023-12-07 20:21:08 -0300

    footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
+   <>
     <div expand="lg" className="footer-color" fluid>
-      <Container >
-        
+      <Container>
         <Row>
-            <Col lg={6} sm={12}>
-            
-            </Col>
+          <Col lg={6} sm={12}></Col>
           <Col lg={3} sm={12}>
             <ul className="list-unstyled">
               <li>PROYECTOS</li>
@@ -18,16 +17,27 @@ const Footer = () => {
             </ul>
           </Col>
           <Col lg={3} sm={12}>
-            <ul className="list-unstyled">
-              <li>PROYECTO</li>
-              <li>PROYECTO</li>
-              <li>PROYECTO</li>
-              <li>PROYECTO</li>
+            <ul className="list-unstyled d-flex  icon">
+              <li className="mx-2">
+                <FaGithub />
+              </li>
+              <li className="mx-2">
+                <FaLinkedin />
+              </li>
+              <li className="mx-2">
+                <FaFacebook />
+              </li>
+              <li className="mx-2 icon">
+                <FaInstagram />
+              </li>
             </ul>
           </Col>
         </Row>
       </Container>
+   
     </div>
+
+   </>
   );
 };
 
