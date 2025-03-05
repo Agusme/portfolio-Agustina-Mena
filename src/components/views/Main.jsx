0f100964e0cfcabda0e6c922275873cc319f648a
agusmena <agusmena60@mail.com>
2025-03-05 10:15:17 -0300

    add changes

--- src/components/views/Main.jsx
+++ src/components/views/Main.jsx
@@ -3,22 +3,25 @@ import { Col, Container, Row } from "react-bootstrap";
 import fotoper from "../../assets/fotoper.png";
 import { useTranslation } from "react-i18next";
 const Main = () => {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
-  const {im, profession,cv} = t("description");
-  
+  const { im, profession, cv } = t("description");
+
   return (
-    <div  id="main">
+    <div id="main">
       <Container fluid className=" bg-main">
         <Row className="d-flex align-items-center  pb-5">
           <Col className="p-2 px-5 mt-5 text-center" lg={7} md={6} sm={12}>
-            <h1 className="fw-bold display-1 text-white">
-{t("greeting")} 
-            </h1>
+            <h1 className="fw-bold display-1 text-white">{t("greeting")}</h1>
             <p className="fw-bold text-white fs-1">{im} </p>
-            <p className="text-white fw-semibold mt-2">
-            {profession} </p>
-            <a href="https://www.canva.com/design/DAGgIbNBCLw/Y2toKtjUXJkc5-t89XzNkg/edit?utm_content=DAGgIbNBCLw&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">{cv} </a>
+            <p className="text-white fw-semibold mt-2">{profession} </p>
+            <a
+              className="btn mt-3 btn-light"
+              href="https://www.canva.com/design/DAGgIbNBCLw/wJOMVsowGH78tUccmbPopg/view?utm_content=DAGgIbNBCLw&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h7fa68bfac6"
+    target="_blank" rel="noreferrer"
+           >
+              {cv}{" "}
+            </a>
           </Col>
           <Col
             className="d-flex align-items-center justify-content-center my-5 pt-5"
