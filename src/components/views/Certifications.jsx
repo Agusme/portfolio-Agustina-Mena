dc6d49bd96937ff02b0c6f2067cffe594bbacfa3
agusmena <agusmena60@mail.com>
2025-03-14 16:14:40 -0300

    add a new certification

--- src/components/views/Certifications.jsx
+++ src/components/views/Certifications.jsx
@@ -4,8 +4,9 @@ import certCodo from "../../assets/certificados/certCodo.jpg";
 import google from "../../assets/certificados/googlw.jpg";
 import alura from "../../assets/certificados/alura.jpg";
 import uxResearch from "../../assets/certificados/ux-research.png";
-import certBackend from "../../assets/certificados/cert-Backend.png"
+import certBackend from "../../assets/certificados/cert-Backend.png";
 import certRolling from "../../assets/certificados/certRolling.jpg";
+import linkedin from "../../assets/certificados/linkedin.png";
 import { Container } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
@@ -31,83 +32,90 @@ const Certifications = () => {
     },
   };
 
-  const {t} = useTranslation();
-
+  const { t } = useTranslation();
 
   return (
-    <div className="bg-projects p-5 text-secondary"   id="certifications">
+    <div className="bg-projects p-5 text-secondary" id="certifications">
       <Fade>
-      <Container>
-        <h2 className="fw-bold  fs-1 text-center">{t("certificationTittle")} </h2>
-        <Carousel responsive={responsive} className="bg-glass py-5">
-        <div className="text-center container">
-            <img
-              src={certBackend}
-              alt="certificado Backend Rolling Code"
-              className="img-fluid w-100 p-3"
-            />
-            <h6 className="text-center fw-bold">Rolling Code</h6>
-            <p>Backend database integration in web applications </p>
-          </div>
-        <div className="text-center container ">
-            <img
-              src={certRolling}
-              alt="certificado Rolling Code"
-              className="img-fluid w-100 p-3"
-            />
-            <h6 className="text-center fw-bold">Rolling Code</h6>
-            <p>Fullstack web programming</p>
-          </div>
-          <div className="text-center container">
-            <img
-              src={uxResearch}
-              alt="certificado Ux Reasearch"
-              className="img-fluid w-100 p-3"
-            />
-            <h6 className="text-center fw-bold">Codo a Codo</h6>
-            <p>Ux Research</p>
-          </div>
-          <div className="text-center container">
-            <img
-              src={prof}
-              alt="certificado CertiProf"
-              className="img-fluid w-100 p-3"
-            />
-            <h6 className="text-center fw-bold">CertiProf</h6>
-            <p>Scrum</p>
-          </div>
-          <div className="text-center container" >
-            <img
-              src={certCodo}
-              alt="certificado Codo a codo"
-              className="img-fluid w-100 p-3"
-            />
-            <h6 className="text-center fw-bold">Codo a codo</h6>
-            <p>Initial programming</p>
-          </div>
-          <div className="text-center container">
-            <img
-              src={alura}
-              alt="certificado alura"
-              className="img-fluid w-100 p-3"
-            />
-            <h6 className="text-center fw-bold">Alura Latam</h6>
-            <p>Javascript</p>
-          </div>
-          <div className="text-center container">
-            <img
-              src={google}
-              alt="certificado Google Activate"
-              className="img-fluid w-100 p-3 "
-            />
-            <h6 className="text-center fw-bold">Google Activate</h6>
-            <p>Introduction to web development</p>
-          </div>
-       
-        </Carousel>
-
- 
-      </Container>
+        <Container>
+          <h2 className="fw-bold  fs-1 text-center">
+            {t("certificationTittle")}{" "}
+          </h2>
+          <Carousel responsive={responsive} className="bg-glass py-5">
+            <div className="text-center container">
+              <img
+                src={certBackend}
+                alt="certificado Backend Rolling Code"
+                className="img-fluid w-100 p-3"
+              />
+              <h6 className="text-center fw-bold">Rolling Code</h6>
+              <p>Backend database integration in web applications </p>
+            </div>
+            <div className="text-center container">
+              <img
+                src={linkedin}
+                alt="certificado React LinkedIn Learning"
+                className="img-fluid w-100 p-3"
+              />
+              <h6 className="text-center fw-bold">LinkedIn Learning</h6>
+              <p>React Esencial </p>
+            </div>
+            <div className="text-center container ">
+              <img
+                src={certRolling}
+                alt="certificado Rolling Code"
+                className="img-fluid w-100 p-3"
+              />
+              <h6 className="text-center fw-bold">Rolling Code</h6>
+              <p>Fullstack web programming</p>
+            </div>
+            <div className="text-center container">
+              <img
+                src={uxResearch}
+                alt="certificado Ux Reasearch"
+                className="img-fluid w-100 p-3"
+              />
+              <h6 className="text-center fw-bold">Codo a Codo</h6>
+              <p>Ux Research</p>
+            </div>
+            <div className="text-center container">
+              <img
+                src={prof}
+                alt="certificado CertiProf"
+                className="img-fluid w-100 p-3"
+              />
+              <h6 className="text-center fw-bold">CertiProf</h6>
+              <p>Scrum</p>
+            </div>
+            <div className="text-center container">
+              <img
+                src={certCodo}
+                alt="certificado Codo a codo"
+                className="img-fluid w-100 p-3"
+              />
+              <h6 className="text-center fw-bold">Codo a codo</h6>
+              <p>Initial programming</p>
+            </div>
+            <div className="text-center container">
+              <img
+                src={alura}
+                alt="certificado alura"
+                className="img-fluid w-100 p-3"
+              />
+              <h6 className="text-center fw-bold">Alura Latam</h6>
+              <p>Javascript</p>
+            </div>
+            <div className="text-center container">
+              <img
+                src={google}
+                alt="certificado Google Activate"
+                className="img-fluid w-100 p-3 "
+              />
+              <h6 className="text-center fw-bold">Google Activate</h6>
+              <p>Introduction to web development</p>
+            </div>
+          </Carousel>
+        </Container>
       </Fade>
     </div>
   );
